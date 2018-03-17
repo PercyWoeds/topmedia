42159c38d31072b0f1d54a8f0b3978a4879460ea
Percy Woeds <percywoeds@gmail.com>
2018-03-17 15:00:12 +0000

    a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   include ActionView::Helpers::NumberHelper
 
-  include FontAwesome::Rails::IconHelper
+  
 
   def check_if_true(item)
   if(item == 'true' or item == true or item == 1 or item == '1')
