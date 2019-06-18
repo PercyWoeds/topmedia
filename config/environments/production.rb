46d3e1ec0de1cc9b9a4c3c3bcfa40c76bd20bd6c
Percy Woeds <percywoeds@gmail.com>
2019-06-18 10:34:36 -0500

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,7 @@ Mnygo::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
    
-   config.serve_static_files = false
-   config.serve_static_assets = true
-   
-    
+   config.serve_static_files = true 
    config.log_level = :info
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
