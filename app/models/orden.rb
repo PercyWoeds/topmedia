e601332c3eb084c65dd0100621e6c0b6c910e7c2
Percy <percywoeds@gmail.com>
2019-07-12 12:44:53 -0500

    a

--- app/models/orden.rb
+++ app/models/orden.rb
@@ -498,9 +498,9 @@ TABLE_HEADERS2 = ["Nº",
 
   end  
   
-  def get_ordenes_detalle(fecha1,fecha2,customer,medio,secuencia,moneda,mes)
+  def get_ordenes_detalle(fecha1,fecha2,customer,medio,secuencia,moneda)
 
-    @orden =Orden.where(["fecha >= ? and fecha <= ? and customer_id=? and medio_id=? and secu_cont =? and moneda_id=? and month=? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59",customer,medio,secuencia,moneda,mes ]).order(:month,:code )
+    @orden =Orden.where(["fecha >= ? and fecha <= ? and customer_id=? and medio_id=? and secu_cont =? and moneda_id=? and month=? ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59",customer,medio,secuencia,moneda ]).order(:month,:code )
     return @orden
   end 
 
