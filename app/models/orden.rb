b5746466e826475c1f289f7ebf47b4355f996db7
Percy <percywoeds@gmail.com>
2019-07-15 12:33:55 -0500

    a

--- app/models/orden.rb
+++ app/models/orden.rb
@@ -444,9 +444,9 @@ TABLE_HEADERS2 = ["Nº",
   def get_moneda(id)
       texto = ""
       if id == 1
-          texto = "SOLES"
-      else
           texto = "DOLARES"
+      else
+          texto = "SOLES"
       end 
       
       return texto 
@@ -510,7 +510,7 @@ TABLE_HEADERS2 = ["Nº",
 
     @orden =Orden.where(["fecha >= ? and fecha <= ? and customer_id=? and medio_id=? and secu_cont =? and moneda_id=?  ", "#{fecha1} 00:00:00","#{fecha2} 23:59:59",customer,medio,secuencia,moneda ]).order(:month,:code )
     return @orden
-    
+
   end 
 
     def self.import(file)
