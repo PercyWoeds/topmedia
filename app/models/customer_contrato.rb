001d418c190e2cbb846ea90f0341d2de79040496
Percy <percywoeds@gmail.com>
2019-07-18 10:46:14 -0500

    a

--- app/models/customer_contrato.rb
+++ app/models/customer_contrato.rb
@@ -1,2 +1,12 @@
 class CustomerContrato < ActiveRecord::Base
+validates_presence_of :secu_cont, :customer_id, :contrato_id,:medio_id, :moneda_id 
+  
+  belongs_to :moneda
+  
+  has_many :customers 
+  has_many :contratos
+  has_many :medios
+  
+
+
 end
