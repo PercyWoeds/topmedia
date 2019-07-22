eb91fa19f41c7d12c58d1efd8e3762b594622048
Percy <percywoeds@gmail.com>
2019-07-22 10:24:30 -0500

    a

--- app/controllers/ordens_controller.rb
+++ app/controllers/ordens_controller.rb
@@ -1851,7 +1851,7 @@ def foot_data_headers_1
     def orden_params
 
     params.require(:orden).permit(:contrato_id,:fecha,:medio_id,:marca_id,:version_id,:producto_id,:fecha1,:fecha2,:tiempo,  
-    :code,:company_id,:subtotal,:tax,:total,:user_id,:processed,:customer_id,:description,:d01,:d02,:d03,:d04,:d05,:d06,:d07,:d08,:d09,:d10,:d11,:d12,:d13,:d14,:d15,:d16,:d17,:d18,:d19,:d20,:d21,:d22,:d23,:d24,:d25,:d26,:d27,:d28,:d29,:d30,:d31,:revision,:ciudad_id,:fecha_inicio,:fecha_fin,:tarifa,:month,:year )
+    :code,:company_id,:subtotal,:tax,:total,:user_id,:processed,:customer_id,:description,:d01,:d02,:d03,:d04,:d05,:d06,:d07,:d08,:d09,:d10,:d11,:d12,:d13,:d14,:d15,:d16,:d17,:d18,:d19,:d20,:d21,:d22,:d23,:d24,:d25,:d26,:d27,:d28,:d29,:d30,:d31,:revision,:ciudad_id,:fecha_inicio,:fecha_fin,:tarifa,:month,:year,:secu_cont  )
     end
 
 end
