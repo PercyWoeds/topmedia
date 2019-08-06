a894775e5798eb7bd5b2fb7984698fd82bf6e2cb
Percy <percywoeds@gmail.com>
2019-08-06 11:17:02 -0500

    a

--- public/javascripts/application3.js
+++ public/javascripts/application3.js
@@ -212,6 +212,7 @@
   }
 
 
+
   // Add an item to a product kit
   function addItemToInvoice() {
     var item = $("#ac_item").val();
@@ -642,6 +643,7 @@
       });
     });
   }
+
 
   // Create new customer in the invoice via ajax
   function doCreateCustomerInvoice() {
@@ -1939,6 +1941,86 @@ function createMarca() {
       });
     } else {
       alert("Por favor ingrese una marca.");
+    }
+  }
+
+
+
+
+  // Shortcut to create new customer form
+  function createOrdenItem() {
+    var orden_id = $("#orden_id").val();
+    
+    $.get('/ordens/new/' + orden_id + '/orden_products/new'+ '?ajax=1', {
+    },
+    function(data) {
+      displayRemote(data);
+      showRemote();
+      
+      $("#new_orden").bind("submit", function() {
+        event.preventDefault();
+        doCreateOrdenItem();
+      });
+    });
+  }
+
+
+  // Create new customer in the invoice via ajax
+  function doCreateOrdenItem() {
+    var company_id = $("#orden_company_id").val();
+    var name = $("#orden_name").val();
+    var email = $("#orden_email").val();
+    var account = $("#orden_account").val();
+    var phone1 = $("#orden_phone1").val();
+    var phone2 = $("#orden_phone2").val();
+    var address1 = $("#orden_address1").val();
+    var address2 = $("#orden_address2").val();
+    var city = $("#orden_city").val();
+    var state = $("#orden_state").val();
+    var zip = $("#orden_zip").val();
+    var country = $("#orden_country").val();
+    var comments = $("#customer_comments").val();
+    
+    if($("#customer_taxable").attr("checked")) {
+      var taxable = 1;
+    } else {
+      var taxable = 0;
+    }
+    
+    if(name != "") {
+      $.post('/customers/create_ajax/' + company_id, {
+        name: name,
+        email: email,
+        account: account,
+        phone1: phone1,
+        phone2: phone2,
+        address1: address1,
+        address2: address2,
+        city: city,
+        state: state,
+        zip: zip,
+        country: country,
+        comments: comments,
+        taxable: taxable
+      },
+      function(data) {
+        if(data == "error_empty") {
+          alert("Please enter a customer name");
+        } else if(data == "error") {
+          alert("Something went wrong when saving the customer, please try again");
+        } else {
+          var data_arr = data.split("|BRK|");
+          
+          $("#ac_customer").val(data_arr[1]);
+          $("#ac_customer_id").val(data_arr[0]);
+          $("#selected_customer").html(data_arr[1]);
+          
+          hideRemote();
+          alert("The customer has been created");
+        }
+      });
+    } else {
+      alert("Please enter a customer name.");
     }
   }
 
