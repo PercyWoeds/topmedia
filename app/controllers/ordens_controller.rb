4bb2921b57f68703c98ff939c44361826c642cf4
Percy <percywoeds@gmail.com>
2019-11-12 11:17:25 -0500

    pdf

--- app/controllers/ordens_controller.rb
+++ app/controllers/ordens_controller.rb
@@ -606,7 +606,7 @@ class OrdensController < ApplicationController
      
 
 
-    Prawn::Document.generate "app/pdf_output/rpt_orden2.pdf" , :page_layout => :landscape,:size=> "A4",:margin => [10,10,5,10] do |pdf|        
+    Prawn::Document.generate "app/pdf_output/rpt_orden2.pdf" , :page_layout => :landscape,:size=> "A4",:margin => [2,2,5,2] do |pdf|        
     
         pdf.font "Helvetica"
         pdf = build_pdf_header_rpt2(pdf)
