09582d7a91f9231c0af00ca7eec25ed8ca0a0b26
Percy Woeds <percywoeds@gmail.com>
2022-01-04 19:45:13 -0500

    tipo aviso

---
+++ db/migrate/20220105004454_create_tipo_avisos.rb
@@ -0,0 +1,10 @@
+class CreateTipoAvisos < ActiveRecord::Migration
+  def change
+    create_table :tipo_avisos do |t|
+      t.string :code
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
