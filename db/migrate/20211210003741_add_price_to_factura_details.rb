e39c505e4996d22cce85e4ef95427a6d249407a9
Percy Woeds <percywoeds@gmail.com>
2022-01-24 11:39:38 -0500

    facturas

--- db/migrate/20211210003741_add_price_to_factura_details.rb
+++
@@ -1,9 +0,0 @@
-class AddPriceToFacturaDetails < ActiveRecord::Migration
-  def change
-    add_column :factura_details, :price, :float
-    add_column :factura_details, :quantity, :float
-    add_column :factura_details, :discount, :float
-
-
-  end
-end
